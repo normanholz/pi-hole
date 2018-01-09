2b8fc333f35984eaa99479a52dadb2236d7edb97
Norman Holz <normanholz@gmail.com>
2018-01-09 20:32:06 +0100

    final layout

--- pi_hole_stats.py
+++ pi_hole_stats.py
@@ -59,16 +59,14 @@ while True:
     piHoleData = []
     piHoleData.append(['IP (Add this as DNS)', str(IP).strip()])
     piHoleData.append(["Clients", str(CLIENTS)])
-    piHoleData.append(["Domains", str(DOMAINS)])
-    piHoleData.append(["Domains blocked", str(BLOCKEDDOMAINS)])
-
+    # piHoleData.append(["Domains", str(DOMAINS)])
+    # piHoleData.append(["Domains blocked", str(BLOCKEDDOMAINS)])
     piHoleData.append(["DNS Queries", str(DNSQUERIES)])
 
     # QUERIES_FORWARDED = data['queries_forwarded']
     # QUERIES_CACHED = data['queries_cached']
-
-    piHoleData.append(["Ads % blocked today", str(ADSTODAY).strip() + " %"])
-    piHoleData.append(["Ads blocked today", str(ADSBLOCKED)])
+    piHoleData.append(["Ads blocked", str(ADSBLOCKED)])
+    piHoleData.append(["Ads blocked (%)", str(ADSTODAY).strip() + " %"])
 
     piHoleDataTable = AsciiTable(piHoleData)
     piHoleDataTable.inner_heading_row_border = False
@@ -79,8 +77,8 @@ while True:
     os.system('clear')
     print("")
     print(piHoleDataTable.table)
-    # print("")
-    # print(piDataTable.table)
+    print("")
+    print(piDataTable.table)
 
 
     time.sleep(0.9)
